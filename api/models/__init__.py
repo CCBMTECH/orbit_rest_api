3dedd91e963e903d1e31a7933c14798df89c6dd9
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-15 14:01:06 +0000

    reorganisation des import sur les models

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from . import rest_api_tokens
+# Base system models
 from . import ir_model
+from . import rest_api_tokens
 
-
-from . import company
-from . import product_template
+# Partner and Company
 from . import partner
-from . import payment_details
+from . import company
 
+# Products
+from . import product_template
+from . import pack_product
+
+# Sales and Orders
 from . import sale
 from . import sale_order_mail
-from . import account_payment_preorder
 from . import sale_credit_order_mail
 
-from . import commentaire
+# Payments
+from . import payment_details
+from . import account_payment_preorder
+
+# CRM and Communication
 from . import crm_model
-from . import pack_product
+from . import commentaire
