c12fcc990335c8d9f2f8b4bddf0625cbe2f08a5a
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-15 12:05:58 +0000

    ajout des modules de bases de l'api

--- api/controllers/main.py
+++ api/controllers/main.py
@@ -738,28 +738,27 @@ else:
         from . import cors_assist
         from . import resources
         from . import default_universal_controller
-        from . import precommande_controller
-        from . import commande_controller
-        from . import user_controller
-        from . import facture_controller
-        from . import payment_controller
-        from . import journal_currency_controller
-        from . import mailer_controller
-        from . import reset_password_controller
-        from . import contact_controller
-        from . import product_categorie_controller
-        from . import excel_update_controller
-        from . import module_dev_controller
-        from . import entreprise_controller
-        from . import creditcommande_controller
-        # from . import scriping
-        from . import commentaire_controller
-        from . import pack_product_controller
-        from . import partner_controller
-        from . import terme_recherche_controller
+        
+        # from . import precommande_controller
+        # from . import commande_controller
+        # from . import user_controller
+        # from . import facture_controller
+        # from . import payment_controller
+        # from . import journal_currency_controller
+        # from . import mailer_controller
+        # from . import reset_password_controller
+        # from . import contact_controller
+        # from . import product_categorie_controller
+        # from . import excel_update_controller
+        # from . import module_dev_controller
+        # from . import entreprise_controller
+        # from . import creditcommande_controller
+        
+        # from . import commentaire_controller
+        # from . import pack_product_controller
+        # from . import partner_controller
+        # from . import terme_recherche_controller
 
-        from . import gestionnaire_controller
-        # from . import service_controller
-        # from . import abonnement_controller
-
+        # from . import gestionnaire_controller
+        
 
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,19 +2,18 @@
 
 from . import rest_api_tokens
 from . import ir_model
-from .import company
-# from . import sale_order_account_move
-from . import product_template
-from . import partner
-from . import payment_details
 
-from . import sale
-from . import sale_order_mail
-from . import account_payment_preorder
-from .import sale_credit_order_mail
-# from .import sale_order_cron_credit_mail_remender
-# from .import sale_order_cron_credit_mail_recover
 
-from .import commentaire
-from .import crm_model
-from .import pack_product
+# from . import company
+# from . import product_template
+# from . import partner
+# from . import payment_details
+
+# from . import sale
+# from . import sale_order_mail
+# from . import account_payment_preorder
+# from . import sale_credit_order_mail
+
+# from . import commentaire
+# from . import crm_model
+# from . import pack_product
