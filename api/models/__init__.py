dbb00ba2e4903c1e3d79026081c618d7b3ecbd52
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-16 08:24:59 +0000

    Ajoute des models : pack_product , payment_details , sale

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -11,17 +11,18 @@ from . import company
 # Products
 from . import commentaire
 from . import product_template
-# from . import pack_product
-
-# # Sales and Orders
-# from . import sale
-# from . import sale_order_mail
-# from . import sale_credit_order_mail
-
-# # Payments
-# from . import payment_details
-# from . import account_payment_preorder
+from . import pack_product
 
 # # CRM and Communication
 from . import crm_model
 
+
+
+# # Sales and Orders
+from . import sale
+# from . import sale_order_mail
+# from . import sale_credit_order_mail
+
+# # Payments
+from . import payment_details
+# from . import account_payment_preorder
