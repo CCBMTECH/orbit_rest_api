8d9b4f07b8f1da2cc64ffff0423dd479a9e47afb
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-17 21:28:42 +0000

    ajout de crm_lead_view

--- api/__manifest__.py
+++ api/__manifest__.py
@@ -37,11 +37,11 @@
         'views/ir_model_view.xml',
         'views/pack_views.xml',
         
-        'views/product_template_view.xml',
+        # 'views/product_template_view.xml',
         'views/res_partner_view.xml',
         
         # 'views/ir_cron_data.xml',
-        # 'views/crm_lead_view.xml',
+        'views/crm_lead_view.xml',
 
     ],
     'images': [
