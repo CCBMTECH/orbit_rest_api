685bdd10fa0a207b878a740c50f92308a9843a69
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-18 09:28:31 +0000

    Update __manifest__.py

--- api/__manifest__.py
+++ api/__manifest__.py
@@ -31,7 +31,7 @@
     'data': [
         'security/ir.model.access.csv',
 
-        'data/ir_configparameter_data.xml',
+        # 'data/ir_configparameter_data.xml',
         # 'data/ir_cron_data.xml',
 
         'views/ir_model_view.xml',
