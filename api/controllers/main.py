18674a7c852e40e4be8bf7360ad645f97731fdad
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-18 22:45:32 +0000

    ajout partner_controller

--- api/controllers/main.py
+++ api/controllers/main.py
@@ -755,9 +755,9 @@ else:
         # from . import entreprise_controller
         # from . import creditcommande_controller
         
-        from . import commentaire_controller
+        # from . import commentaire_controller
         # from . import pack_product_controller
-        # from . import partner_controller
+        from . import partner_controller
         # from . import terme_recherche_controller
 
         # from . import gestionnaire_controller
