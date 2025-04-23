88ee8912ad73eac8824c5f253eb113137d1402f5
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-04-23 11:07:10 +0000

    Ajout des models sms et du security

---
+++ api/sms_models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import sms_config
+
+from . import sale_order
+from . import sale_order_message
+
+from . import send_sms
+from . import sms_history
+from . import sms_model
+from . import test_orange_sms
